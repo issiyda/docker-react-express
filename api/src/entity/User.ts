b76a330d9b1ce7fc06664e7988c5dfe20a6f142e
issiy <allmylifewriter@gmail.com>
2023-05-29 21:01:25 +0900

    moduleのインストールとdocker作成

---
+++ api/src/entity/User.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm"
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    firstName: string
+
+    @Column()
+    lastName: string
+
+    @Column()
+    age: number
+
+    @CreateDateColumn()
+    createdAt: Date;
+    
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+}
